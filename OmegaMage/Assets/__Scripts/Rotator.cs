215c7114a2344fa03cfd23c916cc3f1f1ae082d5
JasonRVCC <G00136378@W213-30.academic.raritanval.edu>
2016-04-29 12:56:09 -0400

    Added Background

---
+++ OmegaMage/Assets/__Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour 
+{
+	public float rotation = 5;
+
+	void Update () 
+	{
+		transform.Rotate(new Vector3(0, 0, rotation) * Time.deltaTime);
+	}
+}
